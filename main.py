dedde2af8322e140cac87f288efce69f082c5988
JesseKinsella1 <89218189+JesseKinsella@users.noreply.github.com>
2022-03-08 14:43:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+day = 7
+weight = 165.5
+month = 'March'
+temp = 40
+
+light_is_on = True #or False, makes Boolean
+
+if light_is_on:
+  print("The Light is On!") #only prints if line 6 is set to true. 
+
+if light_is_on:
+  print("The Light is On!")
+  print("So turn it off!") #tabbed over means it will run all code
+
+
+its_past_noon = True
+
+if its_past_noon:
+  print('feed me!')
